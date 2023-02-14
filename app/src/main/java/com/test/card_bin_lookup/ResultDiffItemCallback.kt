df91d17a6b5a11667081ecc4b818ec4b15325d00
Kirill Mokhnatkin <librarian4832@gmail.com>
2023-02-15 02:04:34 +0300

    Значения по умолчанию в классе Result установлена как null где возможно. Изменено форматирование в одном файле

--- app/src/main/java/com/test/card_bin_lookup/ResultDiffItemCallback.kt
+++ app/src/main/java/com/test/card_bin_lookup/ResultDiffItemCallback.kt
@@ -3,7 +3,9 @@ package com.test.card_bin_lookup
 import androidx.recyclerview.widget.DiffUtil
 
 class ResultDiffItemCallback : DiffUtil.ItemCallback<Result>() {
-    override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean = (oldItem.bin == newItem.bin)
+    override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean =
+        (oldItem.bin == newItem.bin)
 
-    override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean = (oldItem == newItem)
+    override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean =
+        (oldItem == newItem)
 }
